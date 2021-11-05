b5c86aee82f3ab63beb42ff656be33a01e7929f6
Ellen Gomes <78104233+EllenCGomes@users.noreply.github.com>
2021-11-05 19:42:24 -0300

    Add files via upload

---
+++ EX02 - odd_numbers.py
@@ -0,0 +1,12 @@
+n = int(input("Enter a positive integer number: "))
+count = 0
+value = 1
+
+print (value)
+
+while count < n-1:
+    value = value + 2
+    count = count + 1
+    print(value)
+    
+   
